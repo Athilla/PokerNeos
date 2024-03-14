63b0c155f206dbe0396151a4bc90cc802d136737
Hervé CADROY <hcadroy@groupeisagri.com>
2024-03-14 20:58:36 +0100

    reactivation

--- modules/dependencies/Core/businessAssembly/Domain/Configuration/Options/IOption.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Threading.Tasks;
-
-namespace Transversals.Business.Core.Domain.Configuration.Options
-{
-    public interface IOption
-    {
-        Task<bool> UpdateValueAsync(string value);
-    }
-}
--- modules/dependencies/UserPermissions/businessAssembly/Application/Factory/IFactoryAuthProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Transversals.Business.UserPermissions.Application.Factory.Abstractions;
-
-namespace Transversals.Business.UserPermissions.Application.Factory
-{
-    public interface IFactoryAuthProvider
-    {
-        IAuthProvider? GetProvider();
-    }
-}
